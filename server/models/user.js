dd7ab05b83f154db67e6912f6e406af34be690ea
HitheshM <hiteshmedavarapu@gmail.com>
2022-11-17 21:38:44 -0500

    Minor changes

--- server/models/user.js
+++ server/models/user.js
@@ -16,17 +16,5 @@ const users = [
   }
 ];
 
-function getAllUsers() {
-  return users;
-}
-
-function login(user) { // {userName: "sda", password: "gsdhjsga"}
-  let cUser = users.filter( u => u.userName === user.userName);
-  
-  if(!cUser[0]) throw Error("Username not found");
-  if(cUser[0].password !== user.password) throw Error("Password incorrect");
-
-  return cUser[0];
-}
-
-module.exports = { getAllUsers, login };
+let getUsers = () => users;
+module.exports = { getUsers };
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,26 +1,14 @@
-const express = require('express');
+const express = require('express'); 
 const User = require('../models/user');
 const router = express.Router();
 
-router
-  .get('/', async (req, res) => {
-    try {
-      const users = await User.getAllUsers();
-      res.send(users);
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
+router.get('/', (req, res) => {
+  try {
+    const users = User.getUsers(); 
+    res.send(users);
+  } catch (err) {
+    res.status(401).send({message: error.message});
+  }
+});
 
-  .post('/login', async (req, res) => {
-    try {
-      let user = await User.login(req.body);
-      res.send({...user, password: undefined})
-    } catch(err) {
-      res.status(401).send({message: err.message});
-    }
-  })
-
-
-  
 module.exports = router;
